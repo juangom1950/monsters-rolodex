b33aff959c337bf6b15ed7c32879722a8691b278
juangom73 <juangom73@gmail.com>
2020-01-23 15:57:15 -0500

    Updates

---
+++ precache-manifest.a283067b6635dd2efb50f1809cadb857.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a86e212dafb50b96692a2a46a4a7a22",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "189dfc5958243e332220",
+    "url": "/monsters-rolodex/static/css/main.192654e0.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "189dfc5958243e332220",
+    "url": "/monsters-rolodex/static/js/main.fd4ef323.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
